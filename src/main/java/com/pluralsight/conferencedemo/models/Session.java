682d54838a6674040913c6ffc396f7e88583c192
Jackie Ponce <jponce18@gmail.com>
2020-12-03 13:39:08 -0700

    update pom

--- src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,10 +1,16 @@
 package com.pluralsight.conferencedemo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import java.util.List;
 
+@Getter
+@Setter
+@Slf4j
 @Entity(name = "sessions")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
@@ -23,45 +29,46 @@ public class Session {
     private List<Speaker> speakers;
 
     public Session() {
+        System.out.println("Initialize");
     }
 
-    public List<Speaker> getSpeakers() {
-        return speakers;
-    }
-
-    public void setSpeakers(List<Speaker> speakers) {
-        this.speakers = speakers;
-    }
-
-    public Long getSession_id() {
-        return session_id;
-    }
-
-    public void setSession_id(Long session_id) {
-        this.session_id = session_id;
-    }
-
-    public String getSession_name() {
-        return session_name;
-    }
-
-    public void setSession_name(String session_name) {
-        this.session_name = session_name;
-    }
-
-    public String getSession_description() {
-        return session_description;
-    }
-
-    public void setSession_description(String session_description) {
-        this.session_description = session_description;
-    }
-
-    public Integer getSession_length() {
-        return session_length;
-    }
-
-    public void setSession_length(Integer session_length) {
-        this.session_length = session_length;
-    }
+//    public List<Speaker> getSpeakers() {
+//        return speakers;
+//    }
+//
+//    public void setSpeakers(List<Speaker> speakers) {
+//        this.speakers = speakers;
+//    }
+//
+//    public Long getSession_id() {
+//        return session_id;
+//    }
+//
+//    public void setSession_id(Long session_id) {
+//        this.session_id = session_id;
+//    }
+//
+//    public String getSession_name() {
+//        return session_name;
+//    }
+//
+//    public void setSession_name(String session_name) {
+//        this.session_name = session_name;
+//    }
+//
+//    public String getSession_description() {
+//        return session_description;
+//    }
+//
+//    public void setSession_description(String session_description) {
+//        this.session_description = session_description;
+//    }
+//
+//    public Integer getSession_length() {
+//        return session_length;
+//    }
+//
+//    public void setSession_length(Integer session_length) {
+//        this.session_length = session_length;
+//    }
 }
